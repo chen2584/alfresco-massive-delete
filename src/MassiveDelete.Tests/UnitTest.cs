8394b9e6081395cfe0fc7dc797c8ed1972b98e4c
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2019-06-16 19:00:32 +0700

    add unit test

---
+++ src/MassiveDelete.Tests/UnitTest.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Reflection;
+using MassiveDelele.Models;
+using MassiveDelete.Services;
+using Xunit;
+
+namespace MassiveDelete.Tests
+{
+    public class UnitTest
+    {
+        readonly AlfrescoService alfrescoService;
+        readonly AppSetting setting;
+
+        public UnitTest()
+        {
+            this.setting = new AppSetting
+            {
+                Alfresco = new AlfrescoSetting
+                {
+                    UserName = "admin",
+                    Password = "password"
+                }
+            };
+            this.alfrescoService = new AlfrescoService(this.setting, null, null);
+        }
+
+        [Fact]
+        public void GetAuthenticationTest()
+        {
+            var method = alfrescoService.GetType().GetMethod("GetAuthenticationToken", BindingFlags.NonPublic | BindingFlags.Instance);
+            var token = (string)method.Invoke(alfrescoService, null);
+            Assert.Equal("YWRtaW46cGFzc3dvcmQ=", token);
+        }
+    }
+}
